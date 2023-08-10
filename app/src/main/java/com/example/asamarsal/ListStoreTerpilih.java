7939fee7eb7bbcdb5ac28e42cd4010aa44fee649
ujangmental <asamarsal@gmail.com>
2023-08-10 23:08:06 +0700

    Done

---
+++ app/src/main/java/com/example/asamarsal/ListStoreTerpilih.java
@@ -0,0 +1,14 @@
+package com.example.asamarsal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListStoreTerpilih extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_store_terpilih);
+    }
+}
